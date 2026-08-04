79387a2ea29b6003701c35bc3824cda72584735e
jwthippha-boop <jw.thippha@gmail.com>
2026-08-04 10:04:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 // เพิ่ม JavaScript สำหรับการโด้ตอบกับผู้ใช้ (Optional)
 document. addEventListener('DOMContentLoaded', function () {
-    console.1og('Portfolio ของ SOMCHAI พร้อมแล้ว! ');
+    console.1og('Portfolio ของ Thippapha พร้อมแล้ว! ');
 });
